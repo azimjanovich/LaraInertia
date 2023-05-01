0a78e61ff1ed003fecadc717294e17daad5be498
Javlonbek Tuychiyev <president7566@gmail.com>
2023-05-01 15:49:57 +0500

    first commit

---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,96 @@
+<script setup>
+import Checkbox from '@/Components/Checkbox.vue';
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+import GmailLogo from '@/Components/GmailLogo.vue';
+import GitGubLogo from '@/Components/GitHubLogo.vue';
+import { Head, Link, useForm } from '@inertiajs/vue3';
+import GitHubLogo from '@/Components/GitHubLogo.vue';
+
+defineProps({
+    canResetPassword: {
+        type: Boolean,
+    },
+    status: {
+        type: String,
+    },
+});
+
+const form = useForm({
+    email: '',
+    password: '',
+    remember: false,
+});
+
+const submit = () => {
+    form.post(route('login'), {
+        onFinish: () => form.reset('password'),
+    });
+};
+</script>
+
+<template>
+    <GuestLayout>
+
+        <Head title="Log in" />
+
+        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+            {{ status }}
+        </div>
+
+        <form @submit.prevent="submit">
+            <div>
+                <InputLabel for="email" value="Email" />
+
+                <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus
+                    autocomplete="username" />
+
+                <InputError class="mt-2" :message="form.errors.email" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="password" value="Password" />
+
+                <TextInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required
+                    autocomplete="current-password" />
+
+                <InputError class="mt-2" :message="form.errors.password" />
+            </div>
+
+            <div class="block mt-4">
+                <label class="flex items-center">
+                    <Checkbox name="remember" v-model:checked="form.remember" />
+                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <Link v-if="canResetPassword" :href="route('password.request')"
+                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Forgot your password?
+                </Link>
+
+                <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                    Log in
+                </PrimaryButton>
+            </div>
+
+            <div class="flex items-center justify-center mt-4">
+                <a :href="route('auth.google', { provider: 'google' })" class="mr-5">
+                    <GmailLogo
+                        class="w-10 h-10 rounded-full border p-1 border-blue-500 hover:bg-slate-200/90 hover:border-blue-600 duration-300" />
+                </a>
+                <a :href="route('auth.google', { provider: 'google' })">
+                    <GitHubLogo
+                        class="w-10 h-10 rounded-full border p-1 border-blue-500 hover:bg-slate-200/90 hover:border-blue-600 duration-300" />
+                </a>
+            </div>
+
+
+
+        </form>
+    </GuestLayout>
+</template>
